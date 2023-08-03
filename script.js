ecdefa9625b0fccc911cfa92592ce6f9cb640f29
Lilololl <121103034+Lilololl@users.noreply.github.com>
2023-08-03 18:34:07 +0200

    update js

--- script.js
+++ script.js
@@ -9,10 +9,22 @@ $(function() {
     $(this).find('.subpages').toggle();
   });
 
-  // Add the following code for the FAQ section
-  $('.faq-question-container').click(function() {
-    $(this).find('.faq-question').toggleClass('open');
-    $(this).siblings('.faq-answer').toggle();
+  // Add click event listener to direct children of the wrapper
+  $('.about-section-wrapper > .about-section').click(function(event) {
+    // Get the ID of the clicked "About" section
+    const aboutID = $(this).attr('id');
+    // Redirect based on the clicked section's ID
+    if (aboutID === 'about1') {
+      window.location.href = 'information.html';
+    } else if (aboutID === 'about2') {
+      window.location.href = 'casefund.html';
+    } else if (aboutID === 'about3') {
+      window.location.href = 'information.html';
+    }
+  });
+
+  $('.faq-question').click(function() {
+    $(this).toggleClass('open');
+    $(this).next('.faq-answer').toggleClass('show');
   });
 });
-
