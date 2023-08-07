1fff1de4a8aff3fcc18c2878d68e7071e9f5fa32
Lilololl <121103034+Lilololl@users.noreply.github.com>
2023-08-07 18:34:10 +0200

    Add files via upload

---
+++ JS/footer.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the footer element
+    const footer = document.createElement('div');
+    footer.innerHTML = `
+      <div class="contact">
+        <h2>Contact Us</h2>
+        <form action="https://formspree.io/f/myyqegby" method="POST">
+          <div class="label-container">
+            <label>
+              Your email:
+              <input type="email" name="email">
+            </label>
+            <label>
+              Your message:
+              <textarea name="message"></textarea>
+            </label>
+          </div>
+          <button type="submit">Send</button>
+        </form>
+        
+        <p>Email: lautenschlagerlinus@gmail.com</p>
+        <a href="faq.html">FAQ</a>
+      </div>
+  
+      <!-- Footer -->
+      <footer class="footer-section">
+        <div class="footer-content">
+          <p>&copy; 2023 Luti Wealth Management. All rights reserved.</p>
+        </div>
+      </footer>
+    `;
+  
+    // Append the footer to the body
+    document.body.appendChild(footer);
+  });
+  
