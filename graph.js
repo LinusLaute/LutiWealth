671bfc2f4c3372891d459aced52c971d075d6360
Lilololl <121103034+Lilololl@users.noreply.github.com>
2024-04-02 17:26:01 +0200

    ADD POINTS

--- graph.js
+++ graph.js
@@ -31,7 +31,6 @@ const performanceData = {
         backgroundColor: "rgba(75, 192, 192, 0.2)",
         borderColor: "rgba(75, 192, 192, 1)",
         borderWidth: 1,
-        pointRadius: 0 // Set pointRadius to 0 to remove data points
     }]
 };
 
