f972390e768eaeb0ff995ac0e742f7bbf5520d4b
Lilololl <121103034+Lilololl@users.noreply.github.com>
2024-04-02 17:21:02 +0200

    remove data points

--- graph.js
+++ graph.js
@@ -30,7 +30,8 @@ const performanceData = {
         data: prices,
         backgroundColor: "rgba(75, 192, 192, 0.2)",
         borderColor: "rgba(75, 192, 192, 1)",
-        borderWidth: 1
+        borderWidth: 1,
+        pointRadius: 0 // Set pointRadius to 0 to remove data points
     }]
 };
 
